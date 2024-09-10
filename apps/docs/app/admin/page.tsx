f1e26627d03e1812719245418c090459a591750e
prasadbylapudi <prasadbylapudi123@gmail.com>
2024-09-10 16:25:02 +0530

    admin page added

---
+++ apps/docs/app/admin/page.tsx
@@ -0,0 +1,11 @@
+
+import { Admin } from '@repo/ui/admin'
+
+export default function() { 
+    return (
+        <div>
+            <h1>Docs Admin Page</h1>
+            <Admin />
+        </div>
+    )
+}
---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import {Admin} from '@repo/ui/admin'
+
+export default function () { 
+    return (
+        <div>
+            <h1>Hi from web Admin Page</h1>
+            <Admin/>
+        </div>
+    )
+}
